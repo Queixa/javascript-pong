484109ed89c1f76b55a4f69ab8d88421c1aa495c
Queixa <kauan.emanuel.vicente@escola.pr.gov.br>
2024-06-27 17:41:39 -0300

    cria bolinha 27/06/2024

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,9 @@
+//aqui vou criar minha bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -7,5 +13,12 @@ function setup(){
 function draw(){
     //aqui vou por a cor da "mesa"
     background(255,182,193);
+    //chamando a função
+    criabolinha(xBolinha, yBolinha, tamBolinha);
 
+}
+
+//função bolinha
+function criabolinha(xBolinha, yBolinha, tamBolinha){
+    circle (xBolinha, yBolinha, tamBolinha);
 }
